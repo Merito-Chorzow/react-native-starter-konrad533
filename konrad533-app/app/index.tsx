9103305ea891e17128530b3c7c80c57ecaa6fab0
konrad533 <konradkrz586@gmail.com>
2025-10-28 13:12:31 +0100

    Dodanie importów potrzebnych do apki, stworzenie const Notes i dodanie testowych notatek

--- konrad533-app/app/index.tsx
+++ konrad533-app/app/index.tsx
@@ -1,4 +1,18 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { 
+  Text, 
+  View,
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
+
+const Notes = [
+  { id: '1', title: 'Notatka testowa 1', date: '28-10-2025' },
+  { id: '2', title: 'Notatka testowa 2', date: '29-10-2025' },
+  { id: '3', title: 'Notatka testowa 3', date: '30-10-2025' },
+];
 
 export default function Index() {
   return (
@@ -9,7 +23,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello World!</Text>
+      <Text>Moje Notatki</Text>
     </View>
   );
 }
