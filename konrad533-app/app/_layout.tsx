d5d23e8721afedf20945c7dcc16b11ce526d2e6b
konrad533 <konradkrz586@gmail.com>
2025-11-01 11:08:17 +0100

    Zmiana tytułu z index na Notatki

--- konrad533-app/app/_layout.tsx
+++ konrad533-app/app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import React from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'Notatki',
+        }}
+      />
+    </Stack>
+  );
 }
