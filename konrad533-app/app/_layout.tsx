d6462736e9ea717ae5325fa66eb2362f0bf94aac
konrad533 <konradkrz586@gmail.com>
2025-11-01 12:39:06 +0100

    Dodanie przycisku 'Dodaj' który wysuwa nowy ekran z formularzem dodawania nowych notatek

--- konrad533-app/app/_layout.tsx
+++ konrad533-app/app/_layout.tsx
@@ -1,5 +1,6 @@
-import { Stack } from "expo-router";
+import { Stack, Link } from "expo-router";
 import React from "react";
+import { Text, Pressable } from "react-native";
 
 export default function RootLayout() {
   return (
@@ -8,6 +9,28 @@ export default function RootLayout() {
         name="index"
         options={{
           title: 'Notatki',
+          headerRight: () => (
+            <Link href="./add-note" asChild>
+              <Pressable>
+                <Text style={{ fontSize: 16, color: '#007AFF' }}>Dodaj</Text>
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+
+      <Stack.Screen
+        name="add-note"
+        options={{ 
+          title: 'Dodaj Notatkę',
+          presentation: 'modal',
+        }}
+      />
+
+      <Stack.Screen
+        name="note/[id]"
+        options={{ 
+          title: 'Szczegóły Notatki' 
         }}
       />
     </Stack>
